d3cdac26658340e827d600a2b16cc76fba4e2f98
harim1206 <harim1206@gmail.com>
2018-03-21 21:33:07 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,13 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Base
+  def created_at
+    Time.now.utc.to_datetime
+  end
+
+  def updated_at
+    Time.now.utc.to_datetime + 0.0002792
+  end
+
+end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,27 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumestores do |t|
+
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :employees_count
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+
+    end
+  end
+end
+
+#
+# CostumeStore
+#   has a name (FAILED - 1)
+#   has a location (FAILED - 2)
+#   has a costume inventory (FAILED - 3)
+#   has an employees count (FAILED - 4)
+#   knows if it's still in business or permanently closed (FAILED - 5)
+#   has an opening time (FAILED - 6)
+#   has a closing time (FAILED - 7)
